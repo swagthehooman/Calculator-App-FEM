9c65d865459efb69fbca4549e93941fba7551f08
Swagnik Das <104345028+swagthehooman@users.noreply.github.com>
2023-04-05 17:36:36 +0530

    project completion

--- src/components/headingSection/Headings.jsx
+++ src/components/headingSection/Headings.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./HeadingsStyle.css";
+import { to } from "mathjs";
 export default function Headings(props) {
   const newStyle = props.customStyle;
+  const [toggleColor, setToggleColor] = useState(newStyle.backgroundKeypad);
   return (
     <div className="topic" style={{ color: newStyle.heading }}>
       <div className="topic_name">
@@ -9,25 +11,39 @@ export default function Headings(props) {
       </div>
       <div className="topic_setting">
         <p>THEME</p>
-        <div className="setting">
-          <input
-            type="radio"
-            value={0}
-            name="theme"
-            onChange={props.handleRadio}
-          />
-          <input
-            type="radio"
-            value={1}
-            name="theme"
-            onChange={props.handleRadio}
-          />
-          <input
-            type="radio"
-            value={2}
-            name="theme"
-            onChange={props.handleRadio}
-          />
+        <div className="setting_points">
+          <div className="labels">
+            <p>1</p>
+            <p>2</p>
+            <p>3</p>
+          </div>
+          <div
+            className="setting"
+            style={{ backgroundColor: newStyle.backgroundKeypad }}
+          >
+            <input
+              defaultChecked="true"
+              style={{ backgroundColor: newStyle.keyBackgroundEqualsToggle }}
+              type="radio"
+              value={0}
+              name="theme"
+              onChange={props.handleRadio}
+            />
+            <input
+              style={{ backgroundColor: newStyle.keyBackgroundEqualsToggle }}
+              type="radio"
+              value={1}
+              name="theme"
+              onChange={props.handleRadio}
+            />
+            <input
+              style={{ backgroundColor: newStyle.keyBackgroundEqualsToggle }}
+              type="radio"
+              value={2}
+              name="theme"
+              onChange={props.handleRadio}
+            />
+          </div>
         </div>
       </div>
     </div>
